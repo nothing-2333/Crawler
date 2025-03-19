2b66a8787c3befe94cf70e704e449420fdd544d2
nothing <2727219897@qq.com>
2025-03-19 14:23:08 +0800

    简单修正位置

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from Encrypt import Encrypt
 from Env import Env
 
 
-def run(crawler: Crawler, encrypt: Encrypt, env: Env):
+def run(env: Env, crawler: Crawler, encrypt: Encrypt):
     debug = crawler.debug
     debug("请求开始...")
     
@@ -17,12 +17,13 @@ def run(crawler: Crawler, encrypt: Encrypt, env: Env):
     
 
 if __name__ == "__main__":
+    file_path = os.path.join(os.path.abspath(__file__), "..","env", "base.json")
+    env = Env.loadFromJson(file_path)
     session = requests.Session()
     crawler = Crawler(session, logger)
     encrypt = Encrypt(logger)
-    file_path = os.path.join(os.path.abspath(__file__), "..","env", "base.json")
-    env = Env.loadFromJson(file_path)
+
     
-    run(crawler, encrypt, env)
+    run(env, crawler, encrypt)
 
     
